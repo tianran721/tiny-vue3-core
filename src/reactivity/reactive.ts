3c6c4e5b0a7b4e88d80878ce69c2de456508d576
tianran721 <858448643@qq.com>
2022-04-02 11:12:22 +0800

    add : readonly 结构重构

--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,20 +1,12 @@
-import { track, trigger } from "./effect";
-// reactive 返回代理对象
-export function reactive(origin){
-    return new Proxy(origin, {
-        // {foo : 1}
-        get(target, key){
-            const value = Reflect.get(target, key);
-            // 依赖收集
-            track(target,key)
-            return value;
-        },
-        set(target, key, value) {
-            const res = Reflect.set(target, key, value);
-            // 触发依赖
-            trigger(target, key);
-            return res;
-        },
-    });
+import {mutableHandlers,readonlyHandlers} from './baseHandler'
+
+export function reactive(raw){
+    return createReactiveObject(raw,mutableHandlers)
+}
+export function readonly(raw){
+    return createReactiveObject(raw,readonlyHandlers)
 }
 
+function createReactiveObject(target,baseHandlers){
+    return new Proxy(target,baseHandlers);
+}
