2a98b8b90f47a462d4df4fcf4ff413a588cb797b
tianran721 <858448643@qq.com>
2022-04-08 19:20:57 +0800

    add: 初始化element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,10 +1,14 @@
-import { h } from "../../lib/guide-mini-vue.esm.js";
+import {h} from "../../lib/guide-mini-vue.esm.js";
 // App 组件
 export const App = {
     // 假设了用户必须写render
     render() {
-        // ui
-        return h("div", "hi, " + this.msg);
+        return h(
+            "div",
+            { id: "root", class: ["red", "hard"],},
+            // string / array类型
+            [h("p", {class: "red"}, "hi"), h("p", {class: "blue"}, "mini-vue")]
+        );
     },
 
     setup() {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,5 +1,5 @@
 import { createComponentInstance, setupComponent } from "./component";
-
+import { isObject } from "../shared/index";
 export function render(vnode, container) {
     // 修补
     patch(vnode, container);
@@ -10,10 +10,16 @@ function patch(vnode, container) {
     // 是 element 那么就应该处理 element
     // 思考题： 如何去区分是 element 还是 component 类型呢？
     // processElement();
-
-    processComponent(vnode, container);
+    if (typeof vnode.type === "string") {
+        processElement(vnode, container);
+    }
+    else if (isObject(vnode.type)) {
+        processComponent(vnode, container);
+    }
 }
-
+function processElement(vnode, container) {
+    mountElement(vnode, container);
+}
 function processComponent(vnode: any, container: any) {
     mountComponent(vnode, container);
 }
@@ -22,11 +28,36 @@ function mountComponent(vnode: any, container) {
     const instance = createComponentInstance(vnode);
 
     setupComponent(instance);
+
     setupRenderEffect(instance, container);
 }
 
+function mountElement(vnode, container) {
+    var el = document.createElement(vnode.type);
+    var children = vnode.children;
+    // children
+    if (typeof children === "string") {
+        el.textContent = children;
+    }
+    else if (Array.isArray(children)) {
+        mountChildren(vnode, el);
+    }
+    // props
+    var props = vnode.props;
+    for (var key in props) {
+        var val = props[key];
+        el.setAttribute(key, val);
+    }
+    container.append(el);
+}
+
+
+function  mountChildren(vnode, container) {
+    vnode.children.forEach(function (v) {
+        patch(v, container);
+    });
+}
 function setupRenderEffect(instance: any, container) {
     const subTree = instance.render();
-
     patch(subTree, container);
 }
